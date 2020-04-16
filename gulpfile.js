130fb3104f684975fecdcc85085a1e345fcad9af
John Lee <john95lee@gmail.com>
2020-04-16 16:42:36 -0400

    epub work and removed node_modules from repo

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,47 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglifycss = require('gulp-uglifycss');
+var uglify = require('gulp-uglify');
+var pipeline = require('readable-stream').pipeline;
+var browserSync = require('browser-sync').create();
 
-//SASS to CSS---------------------------------
-gulp.task('sass', function(){
-    return gulp.src('./sass/*.scss')
-        .pipe(sass().on('error',sass.logError))
-        .pipe(gulp.dest('./css'));
-});
+//SASS to CSS
+function sassTask() {
+    return gulp.src('./src/sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./src/css'));
+}
 
-//CSS minify-----------------------------------
+//JS minify using uglify
+function jsTask() {
+    return gulp.src('./src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./public/js'));
+}
 
-gulp.task('css', async function(){
+//CSS minify with uglifycss
+function cssTask() {
+    return gulp.src('./src/css/*.css')
+        .pipe(uglifycss())
+        .pipe(gulp.dest('./public/css'));
+}
 
-    gulp.src('./css/*.css')
-        .pipe(uglifycss({
-            "maxLineLen": 80,
-            "uglyComments": true
-        }))
-    .pipe(gulp.dest('./public/css/'));
-});
+//Watch for changes and reload browser
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './public'
+        }
+    });
+    gulp.watch('./src/sass/*.scss', sassTask);
+    gulp.watch('./src/js/*.js', jsTask);
+    gulp.watch('./src/css/*.css', cssTask);
+    gulp.watch('./public/*.html').on('change', browserSync.reload);
+    gulp.watch('./public/js/*.js').on('change', browserSync.reload);
+    gulp.watch('./public/css/*.css').on('change', browserSync.reload);
+}
 
-//Watch files---------------------------------
-gulp.task('sass:watch', function(){
-    gulp.watch('./sass/*.scss',['sass']);
-})
-
-gulp.task('css:watch', function(){
-    gulp.watch('./css/*.css',['css']);
-})
+exports.sassTask = sassTask;
+exports.jsTask = jsTask;
+exports.cssTask = cssTask;
+exports.watch = watch;
