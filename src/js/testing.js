d05e5abb8ca2b9b2316142dda5a9059b7029535c
John Lee <john95lee@gmail.com>
2020-04-20 13:10:54 -0400

    prog bar things

--- src/js/testing.js
+++ src/js/testing.js
@@ -1,30 +1,37 @@
+var controls = document.getElementById("controls");
+var currentPage = document.getElementById("current-percent");
+var slider = document.createElement("input");
+var slide = function(){
+        var cfi = book.locations.cfiFromPercentage(slider.value / 100);
+        rendition.display(cfi);
+};
+var mouseDown = false;
+
 // Load the opf
 var book = ePub("./epub/charles-dickens_oliver-twist.epub");
 var rendition = book.renderTo("viewer", {
-  width: "100%",
-  height: 600,
-  spread: "always"
+    width: "100%",
+    height: 600
 });
+
 var displayed = rendition.display();
 
 var title = document.getElementById("title");
 
-//Next button
 var next = document.getElementById("next");
 next.addEventListener("click", function(e){
     rendition.next();
     e.preventDefault();
 }, false);
 
-//Prev button
 var prev = document.getElementById("prev");
 prev.addEventListener("click", function(e){
     rendition.prev();
     e.preventDefault();
 }, false);
 
-//Handling left/right arrow keys
 var keyListener = function(e){
+
     // Left Key
     if ((e.keyCode || e.which) == 37) {
         rendition.prev();
@@ -34,9 +41,23 @@ var keyListener = function(e){
     if ((e.keyCode || e.which) == 39) {
         rendition.next();
     }
+
 };
 rendition.on("keyup", keyListener);
 document.addEventListener("keyup", keyListener, false);
+
+const getCfiFromHref = async (href) => {
+    const id = href.split('#')[1];
+    const item = book.spine.get(href);
+    await item.load(book.load.bind(book));
+    const el = id ? item.document.getElementById(id) : item.document.body;
+    return item.cfiFromElement(el);
+}
+
+var chapNames = [];
+var chapCFI = [];
+
+console.log(chapCFI[1]);
 
 book.ready.then(function(){
     // Load in stored locations from json or local storage
@@ -49,11 +70,10 @@ book.ready.then(function(){
         // Can pass an option number of chars to break sections by
         // default is 150 chars
         return book.locations.generate(1600);
-        console.log(book.locations.percentageFromCfi());
     }
 })
 .then(function(locations){
-        /*controls.style.display = "block";
+        controls.style.display = "block";
         slider.setAttribute("type", "range");
         slider.setAttribute("min", 0);
         slider.setAttribute("max", 100);
@@ -68,19 +88,18 @@ book.ready.then(function(){
         slider.addEventListener("mouseup", function(){
                 mouseDown = false;
         }, false);
-        */
+
         // Wait for book to be rendered to get current page
         displayed.then(function(){
                 // Get the current CFI
                 var currentLocation = rendition.currentLocation();
                 // Get the Percentage (or location) from that CFI
                 var currentPage = book.locations.percentageFromCfi(currentLocation.start.cfi);
-                console.log(currentPage);
-                //slider.value = currentPage;
-                //currentPage.value = currentPage;
+                slider.value = currentPage;
+                currentPage.value = currentPage;
+
         });
 
-/*
         controls.appendChild(slider);
 
         currentPage.addEventListener("change", function(){
@@ -101,31 +120,10 @@ book.ready.then(function(){
 
         // Save out the generated locations to JSON
         localStorage.setItem(book.key()+'-locations', book.locations.save());
-*/
-});
 
-
-window.addEventListener("unload", function () {
-  console.log("unloading");
-  this.book.destroy();
-});
-
-const getCfiFromHref = async (href) => {
-    const id = href.split('#')[1];
-    const item = book.spine.get(href);
-    await item.load(book.load.bind(book));
-    const el = id ? item.document.getElementById(id) : item.document.body;
-    return item.cfiFromElement(el);
-}
-
-var chapNames = [];
-var chapCFI = [];
-
-book.loaded.navigation.then(function(toc, navPath){
-  var $select = document.getElementById("toc"),
-    docfrag = document.createDocumentFragment();
-
-  toc.forEach(function(chapter) {
+})
+.then(function(toc){
+    toc.forEach(function(chapter) {
     var option = document.createElement("option");
     option.textContent = chapter.label;
     option.setAttribute("ref", chapter.href);
@@ -135,51 +133,10 @@ book.loaded.navigation.then(function(toc, navPath){
     getCfiFromHref(chapter.href)
       .then(function(value){
         chapCFI.push(value);
+        console.log(value);
       })
 
-    docfrag.appendChild(option);
-  });
-
-  $select.appendChild(docfrag);
-
-  $select.onchange = function(){
-    var index = $select.selectedIndex,
-            url = $select.options[index].getAttribute("ref");
-    rendition.display(url);
-    return false;
-  };
-
-  return book.locations.generate(1600);
-
 });
-/*
-if (chapNames.length == chapCFI.length){
-  var timeline = document.getElementById("timeline");
-  var i;
-  for (i=0; i < chapNames.length; i++){
-    timeline.innerHTML += "<div id=&quotchap"+i+"&quotclass=&quotmarker&quot data-toggle=&quottooltip&quot title=&quot"+chapNames[i]+"&quot style="left: 0%;"></div>"
-  }
 
-}
-*/
-console.log(chapNames);
-console.log((chapCFI));
 
-console.log(book.locations.percentageFromCfi("epubcfi(/6/6[chapter-1.xhtml]!/"));
 
-//JQuery
-$(function(){
-
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
-
-  $(".circle").mouseenter(function() {
-    $(this).addClass("hover");
-  });
-
-  $(".circle").mouseleave(function() {
-    $(this).removeClass("hover");
-  });
-
-});
